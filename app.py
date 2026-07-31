eb065282a0a61cc14828f7f9bf6f169b39fae5f2
annisarahmaat <ann.rhmtllh@gmail.com>
2026-07-31 09:47:28 +0800

    Update app.py

--- app.py
+++ app.py
@@ -42,8 +42,8 @@ with col1:
     max_y = 15
     
     # A. DAERAH EFISIENSI 
-    x_poly = [1.0, 3.0, 3.0, 2.12, 1.0]
-    y_poly = [3.0, 4.5, 12.0, 12.0, 5.67]
+    x_poly = [1.0, 3.0, 3.0, 1.0]
+    y_poly = [3.0, 4.5, 12.0, 12.0]
 
     ax.fill(x_poly, y_poly, color='#b0a875', alpha=0.75, label='Daerah Efisiensi')
     ax.text(2.1, 7.5, 'Daerah\nEfisiensi', fontsize=11, color='#2c2810', fontweight='bold', 
