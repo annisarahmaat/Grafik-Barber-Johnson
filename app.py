04e85215527e0a1a80d1bdba7a10f12366682989
annisarahmaat <ann.rhmtllh@gmail.com>
2026-08-04 23:21:03 +0800

    Update app.py

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ nama_periode = f"— Periode {periode.strip()}" if periode.strip() != "" else ""
 col1, col2 = st.columns([1.5, 1])
 
 with col1:
-    fig, ax = plt.subplots(figsize=(5, 6), dpi=100)
+    fig, ax = plt.subplots(figsize=(3.3, 4), dpi=100)
     
     max_x = 8
     max_y = 15
