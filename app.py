2bd745ed1e6ce6c3453d58550b868f893619f775
annisarahmaat <ann.rhmtllh@gmail.com>
2026-08-04 23:17:58 +0800

    Update app.py

--- app.py
+++ app.py
@@ -44,10 +44,10 @@ data_lengkap = (bor is not None) and (los is not None) and (toi is not None) and
 nama_periode = f"— Periode {periode.strip()}" if periode.strip() != "" else ""
 
 # 3. LAYOUT UTAMA
-col1, col2 = st.columns([2, 1])
+col1, col2 = st.columns([1.5, 1])
 
 with col1:
-    fig, ax = plt.subplots(figsize=(2.5, 3), dpi=100)
+    fig, ax = plt.subplots(figsize=(5.5, 4.5), dpi=100)
     
     max_x = 8
     max_y = 15
